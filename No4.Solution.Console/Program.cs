b1901906daa4056e187decb69ed78d5d1a2a86f2
MikkBenelis <codemikk@gmail.com>
2018-11-26 11:31:49 +0300

    Implemented solution No4

--- No4.Solution.Console/Program.cs
+++ No4.Solution.Console/Program.cs
@@ -1,9 +1,16 @@
 ﻿namespace No4.Solution.Console
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            IRandomFileGenerator randomFileGenerator;
+            
+            randomFileGenerator = new RandomBytesFileGenerator(@"C:/Users/Mikk/Desktop", ".bytes");
+            randomFileGenerator.GenerateRandomFile(100);
+
+            randomFileGenerator = new RandomCharsFileGenerator(@"C:/Users/Mikk/Desktop", ".chars");
+            randomFileGenerator.GenerateRandomFile(100);
         }
     }
 }
