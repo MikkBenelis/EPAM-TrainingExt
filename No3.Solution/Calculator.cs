6bb1b175f9655cd2c9605b89d4887558828bc353
MikkBenelis <codemikk@gmail.com>
2018-11-26 10:51:40 +0300

    Implemented solution No3

---
+++ No3.Solution/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace No3.Solution
+{
+    public class Calculator
+    {
+        public double CalculateAverage(List<double> values, Func<List<double>, double> averagingFunc)
+        {
+            if (values == null || averagingFunc == null)
+            {
+                throw new ArgumentNullException(nameof(values));
+            }
+
+            return averagingFunc(values);
+        }
+    }
+}
